8608d00b6f319d96f45e2ba443309d0f99e818bc
tats morales <77303353+itsbeesfan@users.noreply.github.com>
2025-10-09 00:03:06 -0500

    add "basic" veredict assets, positioned the assets. added gradient background for "basic" veredict. changed color palette.

--- judging.js
+++ judging.js
@@ -86,6 +86,12 @@ document.getElementById("judge-button").addEventListener("click", () => {
                 { src: "me-assets/newspaper.jpg", top: "10%", right: "10%" },
                 { src: "me-assets/comic.png", bottom: "10%", left: "20%" },
             ],
+            basic: [
+                { src: "basic-assets/disco%20heart.png", top: "10%", left: "5%"},
+                { src: "basic-assets/kiss.png", bottom: "15%", right: "10%" },
+                { src: "basic-assets/matcha.png", top: "10%", right: "10%" },
+                { src: "basic-assets/pink%20stamp.png", bottom: "10%", left: "20%" },
+            ],
             kpop: [
                 { src: "kpop-assets/circle%20asset%20thing.png", top: "10%", left: "5%"},
                 { src: "kpop-assets/flower%20asset.png", bottom: "25%", right: "10%" },
